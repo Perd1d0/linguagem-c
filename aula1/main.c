3065d77a88702a12a9b21155b3f72ed5ffc70283
Jonathan <127849976+Perd1d0@users.noreply.github.com>
2024-09-02 07:37:17 -0300

    Add files via upload

---
+++ aula1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Esse é o primeiro exemplo do curso de linguagem C
+int main(void) {
+  // Exibi as informações no console
+  printf("Meu primeiro trabalhp em C\n");
+  printf("Meu nome é: Jonathan\n");
+  // Sucesso ao compilador o codigo
+  return 0;
+}
